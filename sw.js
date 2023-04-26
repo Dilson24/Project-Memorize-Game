d3596f93e760c38b077aa944b18a46b87325721f
Dilson24 <dilacn97@hotmail.com>
2023-04-26 16:43:20 -0500

    [NEW]-PWA

---
+++ sw.js
@@ -0,0 +1,67 @@
+;
+//asignar un nombre y versión al cache
+const CACHE_NAME = 'v1_Project_Memorize_Game',
+    urlsToCache = [
+        './',
+        './index.html',
+        './Memorize_game.html',
+        './CSS/all.css',
+        './CSS/Style.css',
+        './CSS/Style-game.css',
+        './script.js',
+        './sw.js',
+        './JS/App.js',
+        './JS/SweetAlert.js',
+        './Img/',
+        './Fonts/'
+
+    ]
+
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache)
+                    .then(() => self.skipWaiting())
+            })
+            .catch(err => console.log('Falló registro de cache', err))
+    )
+})
+
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+    const cacheWhitelist = [CACHE_NAME]
+
+    e.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+                return Promise.all(
+                    cacheNames.map(cacheName => {
+                        //Eliminamos lo que ya no se necesita en cache
+                        if (cacheWhitelist.indexOf(cacheName) === -1) {
+                            return caches.delete(cacheName)
+                        }
+                    })
+                )
+            })
+            // Le indica al SW activar el cache actual
+            .then(() => self.clients.claim())
+    )
+})
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+    //Responder ya sea con el objeto en caché o continuar y buscar la url real
+    e.respondWith(
+        caches.match(e.request)
+            .then(res => {
+                if (res) {
+                    //recuperar del cache
+                    return res
+                }
+                //recuperar de la petición a la url
+                return fetch(e.request)
+            })
+    )
+})
